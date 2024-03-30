0723153b59cb4d99f819207803ce2fd36bb224b8
Oluwadabira <adigunsamuelaso@gmail.com>
2024-03-30 13:38:59 +0100

    Setup uploadthing

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        port: 'https',
+        hostname: 'uploadthing.com',
+      },
+      { port: 'https', hostname: 'utfs.io' },
+    ],
+  },
+};
 
 export default nextConfig;
