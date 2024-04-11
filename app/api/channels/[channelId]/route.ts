31b16dd0864a832d490232a5be0ae5569819e3ee
Oluwadabira <adigunsamuelaso@gmail.com>
2024-04-11 18:37:02 +0100

    Edit channel

--- app/api/channels/[channelId]/route.ts
+++ app/api/channels/[channelId]/route.ts
@@ -1,31 +1,31 @@
-import { currentProfile } from "@/lib/current-profile"
-import db from "@/lib/db"
-import { MemberRole } from "@prisma/client"
-import { NextResponse } from "next/server"
+import { currentProfile } from '@/lib/current-profile';
+import db from '@/lib/db';
+import { MemberRole } from '@prisma/client';
+import { NextResponse } from 'next/server';
 
 export async function DELETE(
   req: Request,
-  {params}: {params: {channelId: string}}
-){
+  { params }: { params: { channelId: string } },
+) {
   try {
-    const profile = await currentProfile()
+    const profile = await currentProfile();
 
-    const {searchParams} = new URL(req.url)
+    const { searchParams } = new URL(req.url);
 
-    const serverId = searchParams.get('serverId')
+    const serverId = searchParams.get('serverId');
 
-    if(!profile){
-      return new NextResponse('Unauthorized', {status: 401})
+    if (!profile) {
+      return new NextResponse('Unauthorized', { status: 401 });
     }
 
-    if(!serverId){
-      return new NextResponse('Server Id missing', {status: 400})
+    if (!serverId) {
+      return new NextResponse('Server Id missing', { status: 400 });
     }
 
-    if(!params.channelId){
-      return new NextResponse('Channel Id missing', {status: 400})
+    if (!params.channelId) {
+      return new NextResponse('Channel Id missing', { status: 400 });
     }
-    
+
     const server = await db.server.update({
       where: {
         id: serverId,
@@ -33,27 +33,91 @@ export async function DELETE(
           some: {
             profileId: profile.id,
             role: {
-              in: [MemberRole.ADMIN, MemberRole.MODERATOR]
-            }
-          }
-        }
+              in: [MemberRole.ADMIN, MemberRole.MODERATOR],
+            },
+          },
+        },
       },
       data: {
         channels: {
           delete: {
             id: params.channelId,
             name: {
-              not: 'general'
-            }
-          }
-        }
-      }
-    })
+              not: 'general',
+            },
+          },
+        },
+      },
+    });
 
-    return NextResponse.json(server)
-
+    return NextResponse.json(server);
   } catch (error) {
-    console.log("Channel Id DELETE", error) 
-    return new NextResponse('Internal Error', {status: 500})
+    console.log('Channel Id DELETE', error);
+    return new NextResponse('Internal Error', { status: 500 });
+  }
+}
+
+export async function PATCH(
+  req: Request,
+  { params }: { params: { channelId: string } },
+) {
+  try {
+    const profile = await currentProfile();
+
+    const { name, type } = await req.json();
+    const { searchParams } = new URL(req.url);
+
+    const serverId = searchParams.get('serverId');
+
+    if (!profile) {
+      return new NextResponse('Unauthorized', { status: 401 });
+    }
+
+    if (!serverId) {
+      return new NextResponse('Server Id missing', { status: 400 });
+    }
+
+    if (!params.channelId) {
+      return new NextResponse('Channel Id missing', { status: 400 });
+    }
+
+    if (name === 'general') {
+      return new NextResponse('Name cannot be general', { status: 400 });
+    }
+
+    const server = await db.server.update({
+      where: {
+        id: serverId,
+        members: {
+          some: {
+            profileId: profile.id,
+            role: {
+              in: [MemberRole.ADMIN, MemberRole.MODERATOR],
+            },
+          },
+        },
+      },
+      data: {
+        channels: {
+          update: {
+            where: {
+              id: params.channelId,
+              NOT: {
+                name: 'general',
+              },
+            },
+            data: {
+              name,
+              type,
+            },
+          },
+        },
+      },
+    });
+
+    return NextResponse.json(server);
+  } catch (error) {
+    console.log('Channel Id PATCH', error);
+    return new NextResponse('Internal Error', { status: 500 });
   }
 }
--- components/modals/edit-channel-modal.tsx
+++ components/modals/edit-channel-modal.tsx
@@ -29,7 +29,7 @@ import { Input } from '@/components/ui/input';
 import z from 'zod';
 import { zodResolver } from '@hookform/resolvers/zod';
 import { useForm } from 'react-hook-form';
-import { useParams, useRouter } from 'next/navigation';
+import { useRouter } from 'next/navigation';
 import { useModal } from '@/hooks/use-modal-store';
 import { ChannelType } from '@prisma/client';
 import { useEffect } from 'react';
@@ -44,43 +44,41 @@ const formSchema = z.object({
   type: z.nativeEnum(ChannelType),
 });
 
-export const CreateChannelModal = () => {
+export const EditChannelModal = () => {
   const { isOpen, onClose, type, data } = useModal();
 
   const router = useRouter();
-  const params = useParams();
 
-  const isModalOpen = isOpen && type === 'createChannel';
+  const isModalOpen = isOpen && type === 'editChannel';
 
-  const { channelType } = data;
+  const { server, channel } = data;
 
   const form = useForm({
     resolver: zodResolver(formSchema),
     defaultValues: {
       name: '',
-      type: channelType || ChannelType.TEXT,
+      type: channel?.type || ChannelType.TEXT,
     },
   });
 
   useEffect(() => {
-    if (channelType) {
-      form.setValue('type', channelType);
-    } else {
-      form.setValue('type', ChannelType.TEXT);
+    if (channel) {
+      form.setValue('name', channel.name);
+      form.setValue('type', channel.type);
     }
-  }, [channelType, form]);
+  }, [form]);
 
   const isLoading = form.formState.isSubmitting;
 
   const onSubmit = async (values: z.infer<typeof formSchema>) => {
     try {
       const url = qs.stringifyUrl({
-        url: '/api/channels',
+        url: `/api/channels/${channel?.id}`,
         query: {
-          serverId: params?.serverId,
+          serverId: server?.id,
         },
       });
-      await axios.post(url, values);
+      await axios.patch(url, values);
 
       form.reset();
       router.refresh();
@@ -100,7 +98,7 @@ export const CreateChannelModal = () => {
       <DialogContent className="bg-white text-black p-0 overflow-hidden">
         <DialogHeader className="pt-8 px-6">
           <DialogTitle className="text-center text-2xl font-bold">
-            Create Channnel
+            Edit Channel
           </DialogTitle>
         </DialogHeader>
         <Form {...form}>
@@ -161,7 +159,7 @@ export const CreateChannelModal = () => {
 
             <DialogFooter className="bg-gray-100 px-6 py-4">
               <Button variant="primary" disabled={isLoading}>
-                Create
+                Save
               </Button>
             </DialogFooter>
           </form>
