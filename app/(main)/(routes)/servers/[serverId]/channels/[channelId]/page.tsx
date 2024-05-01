c8c554cd3a07f86399b8981b445d13db6507ced5
Oluwadabira <adigunsamuelaso@gmail.com>
2024-05-01 11:59:48 +0100

    Creating messages in channel

--- app/(main)/(routes)/servers/[serverId]/channels/[channelId]/page.tsx
+++ app/(main)/(routes)/servers/[serverId]/channels/[channelId]/page.tsx
@@ -1,9 +1,10 @@
-import ChatHeader from '@/components/chat/chat-header';
-import { currentProfile } from '@/lib/current-profile';
-import db from '@/lib/db';
-import { redirectToSignIn } from '@clerk/nextjs';
-import { redirect } from 'next/navigation';
-import React from 'react';
+import ChatHeader from "@/components/chat/chat-header";
+import { ChatInput } from "@/components/chat/chat-input";
+import { currentProfile } from "@/lib/current-profile";
+import db from "@/lib/db";
+import { redirectToSignIn } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
+import React from "react";
 
 interface ChannelIdPageProps {
   params: {
@@ -33,15 +34,25 @@ const ChannelIdPage = async ({ params }: ChannelIdPageProps) => {
   });
 
   if (!channel || !member) {
-    return redirect('/');
+    return redirect("/");
   }
 
   return (
-    <div>
+    <div className="bg-white dark:bg-[#313338] flex flex-col h-full">
       <ChatHeader
         name={channel.name}
         serverId={channel.serverId}
         type="channel"
+      />
+      <div className="flex-1">Future Messages</div>
+      <ChatInput
+        name={channel.name}
+        type="channel"
+        apiUrl="/api/socket/messages"
+        query={{
+          channelId: channel.id,
+          serverId: channel.serverId,
+        }}
       />
     </div>
   );
