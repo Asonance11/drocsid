f3beedc4489d24439ccc8bfc67fc7769c89d1dc6
Oluwadabira <adigunsamuelaso@gmail.com>
2024-05-01 10:39:26 +0100

    Socket indicator

--- components/chat/chat-header.tsx
+++ components/chat/chat-header.tsx
@@ -1,12 +1,13 @@
-import { Hash } from 'lucide-react';
-import React from 'react';
-import MobileToggle from '../mobile-toggle';
-import UserAvatar from '@/components/user-avatar';
+import { Hash } from "lucide-react";
+import React from "react";
+import MobileToggle from "../mobile-toggle";
+import UserAvatar from "@/components/user-avatar";
+import { SocketIndicator } from "@/components/socket-indicator";
 
 interface ChatHeaderProps {
   serverId: string;
   name: string;
-  type: 'conversation' | 'channel';
+  type: "conversation" | "channel";
   imageUrl?: string;
 }
 
@@ -14,13 +15,16 @@ const ChatHeader = ({ serverId, name, type, imageUrl }: ChatHeaderProps) => {
   return (
     <div className="text-md font-semibold px-3 flex items-center h-12 border-neutral-200 dark:border-neutral-800 border-b-2 ">
       <MobileToggle serverId={serverId} />
-      {type === 'channel' && (
+      {type === "channel" && (
         <Hash className="h-5 w-5 text-zinc-500 dark:text-zinc-400 mr-2" />
       )}
-      {type === 'conversation' && (
+      {type === "conversation" && (
         <UserAvatar src={imageUrl} className="h-8 w-8 md:h-8 md:w-8 mr-2" />
       )}
       <p className="font-semibold text-md text-black dark:text-white">{name}</p>
+      <div className="ml-auto flex items-center">
+        <SocketIndicator />
+      </div>
     </div>
   );
 };
